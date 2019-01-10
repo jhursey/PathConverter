901c3d1f3afab1004dc574232d6341e9502bfa3f
Corey M Balon <cbalon@gmail.com>
2019-01-10 09:13:56 -0500

    First working version
    Added controller class to send commands to keypad. This is the initial fully functional version

---
+++ csharp/PathConverter/Controller.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PathConverter
+{
+    // Sends commands to keypad and accumulates selections
+    class Controller
+    {
+        public string DecodePath(string keyPath)
+        {
+            KeyPad keyPad = new KeyPad();
+            StringBuilder searchTerm = new StringBuilder();
+            foreach(char ch in keyPath)
+            {
+                switch(ch)
+                {
+                    case char c when c == Commands.UP ||
+                            c == Commands.DOWN ||
+                            c == Commands.LEFT ||
+                            c == Commands.RIGHT:
+                        keyPad.Move(c);
+                        break;
+                    case char c when c == Commands.SPACE:
+                        searchTerm.Append(' ');
+                        break;
+                    case char c when c == Commands.SELECT:
+                        searchTerm.Append(keyPad.Select());
+                        break;
+                    default:
+                        // For now we will do nothing in the case of an unexpected command,
+                        // since there are no explicit requirements for this case
+                        break;
+                }
+            }
+            return searchTerm.ToString();
+        }
+    }
+}
--- csharp/PathConverter/Program.cs
+++ csharp/PathConverter/Program.cs
@@ -13,7 +13,7 @@ namespace PathConverter
         {
             if (args.Length == 0)
             {
-                System.Console.WriteLine("Please enter a filename");
+                Console.WriteLine("Please enter a filename");
                 return 1;
             }
 
@@ -21,13 +21,43 @@ namespace PathConverter
 
             if(!File.Exists(fileName))
             {
-                System.Console.WriteLine("Input file does not exist");
+                Console.WriteLine("Input file does not exist");
                 return 1;
             }
 
-            StreamReader stream = new StreamReader(fileName);
-            //TODO: read file
-            // line = stream.ReadLine();
+            Controller controller = new Controller();
+            List<string> searchTerms = new List<string>();
+            try
+            {
+                using (StreamReader stream = new StreamReader(fileName))
+                {
+                    string keyPath = stream.ReadLine();
+                    string searchTerm = controller.DecodePath(keyPath);
+                    searchTerms.Add(searchTerm);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+
+            string outputFileName = "PathConverterOutput.txt";
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(outputFileName))
+                {
+                    foreach(string searchTerm in searchTerms)
+                    {
+                        writer.Write(searchTerm);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Could not write to file {outputFileName}");
+                Console.WriteLine(e.Message);
+            }
 
             return 0;
         }
