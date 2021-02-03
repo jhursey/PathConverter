a1edbf2a0b9b498d213a05492ed9f59a2576f87d
mstrnisha <74889066+mstrnisha@users.noreply.github.com>
2021-02-02 20:33:45 -0500

    Initial commit

---
+++ PathConverter.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace PathConverter
+{
+    partial class PathConverter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.inputFileTextBox = new System.Windows.Forms.TextBox();
+            this.brwsInputButton = new System.Windows.Forms.Button();
+            this.brwsOutputButton = new System.Windows.Forms.Button();
+            this.outputFileTextBox = new System.Windows.Forms.TextBox();
+            this.convertButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.outputTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // inputFileTextBox
+            // 
+            this.inputFileTextBox.Location = new System.Drawing.Point(29, 53);
+            this.inputFileTextBox.Name = "inputFileTextBox";
+            this.inputFileTextBox.Size = new System.Drawing.Size(487, 22);
+            this.inputFileTextBox.TabIndex = 0;
+            // 
+            // brwsInputButton
+            // 
+            this.brwsInputButton.Location = new System.Drawing.Point(532, 52);
+            this.brwsInputButton.Name = "brwsInputButton";
+            this.brwsInputButton.Size = new System.Drawing.Size(114, 23);
+            this.brwsInputButton.TabIndex = 1;
+            this.brwsInputButton.Text = "Browse...";
+            this.brwsInputButton.UseVisualStyleBackColor = true;
+            this.brwsInputButton.Click += new System.EventHandler(this.brwsInputButton_Click);
+            // 
+            // brwsOutputButton
+            // 
+            this.brwsOutputButton.Location = new System.Drawing.Point(532, 107);
+            this.brwsOutputButton.Name = "brwsOutputButton";
+            this.brwsOutputButton.Size = new System.Drawing.Size(114, 23);
+            this.brwsOutputButton.TabIndex = 3;
+            this.brwsOutputButton.Text = "Browse...";
+            this.brwsOutputButton.UseVisualStyleBackColor = true;
+            this.brwsOutputButton.Click += new System.EventHandler(this.brwsOutputButton_Click);
+            // 
+            // outputFileTextBox
+            // 
+            this.outputFileTextBox.Location = new System.Drawing.Point(29, 108);
+            this.outputFileTextBox.Name = "outputFileTextBox";
+            this.outputFileTextBox.Size = new System.Drawing.Size(487, 22);
+            this.outputFileTextBox.TabIndex = 2;
+            // 
+            // convertButton
+            // 
+            this.convertButton.Location = new System.Drawing.Point(249, 165);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(175, 36);
+            this.convertButton.TabIndex = 4;
+            this.convertButton.Text = "Convert";
+            this.convertButton.UseVisualStyleBackColor = true;
+            this.convertButton.Click += new System.EventHandler(this.convertButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Input File";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Output File";
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.Enabled = false;
+            this.outputTextBox.Location = new System.Drawing.Point(32, 251);
+            this.outputTextBox.Multiline = true;
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.outputTextBox.Size = new System.Drawing.Size(614, 332);
+            this.outputTextBox.TabIndex = 7;
+            this.outputTextBox.WordWrap = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 231);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Output";
+            // 
+            // PathConverter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(661, 595);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.outputTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.convertButton);
+            this.Controls.Add(this.brwsOutputButton);
+            this.Controls.Add(this.outputFileTextBox);
+            this.Controls.Add(this.brwsInputButton);
+            this.Controls.Add(this.inputFileTextBox);
+            this.Name = "PathConverter";
+            this.Text = "PathConverter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox inputFileTextBox;
+        private System.Windows.Forms.Button brwsInputButton;
+        private System.Windows.Forms.Button brwsOutputButton;
+        private System.Windows.Forms.TextBox outputFileTextBox;
+        private System.Windows.Forms.Button convertButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox outputTextBox;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ PathConverter.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PathConverter
+{
+    /// <summary>
+    /// Windows form used to convert a given path of keys to text.
+    /// Users input search terms using a television remote. The arrow buttons are used to navigate a grid of letters 
+    /// until a letter is selected. The letters are organized as follows:
+    ///     ABCDEF
+    ///     GHIJKL
+    ///     MNOPQR
+    ///     STUVWX
+    ///     YZ1234
+    ///     567890
+    /// 
+    /// This program accepts a flat file as input.  Each line is a new key path.
+    /// Key paths consist of a string of characters where:
+    ///     U = up
+    ///     D = down
+    ///     L = left
+    ///     R = right
+    ///     S = space
+    ///     * = select
+    ///     
+    /// Key paths always start at the upper left corner, i.e. 'A'
+    /// The output is written out to a new file specified by the user
+    /// 
+    /// Sample Input
+    ///     DDDR* UU* LLLU* SDD* DLL* UU* U* RRD* SURRR* LLDD* LLL* DRR* RRRU* SUULL* DDLLL* LLLD* SUULL* DDL* LLU* RRR* UUR* L* SDDL* RD* UUUR* DDR* SRRD* UU* LLLU* SDR* RU* UUR* L* SDDRRR* DDL* LLU*
+    ///
+    /// Sample Output
+    ///     THE QUICK BROWN FOX JUMPED OVER THE LAZY DOG
+    /// </summary>
+    public partial class PathConverter : Form
+    {
+        // multidimensional array holding the remote keys
+        string[,] remoteKeys = new string[6, 6] {{ "A", "B", "C", "D", "E", "F"}, { "G", "H", "I", "J", "K", "L"},
+                                        { "M", "N", "O", "P", "Q", "R"}, { "S", "T", "U", "V", "W", "X"},
+                                        { "Y", "Z", "1", "2", "3", "4"}, { "5", "6", "7", "8", "9", "0"}};
+
+        // holds the current position (row and column)
+        private int currentRow, currentCol = 0;
+
+        public PathConverter()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Used to load the input file text box with what is selected from the OpenFileDialog
+        /// </summary>
+        private void brwsInputButton_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                inputFileTextBox.Text = openFileDialog1.FileName;
+            }
+        }
+
+        /// <summary>
+        /// Used to load the output file text box with what is selected from the OpenFileDialog
+        /// </summary>
+        private void brwsOutputButton_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                outputFileTextBox.Text = openFileDialog1.FileName;
+            }
+        }
+
+        /// <summary>
+        /// Runs the conversion process on the input keys and outputs the converted text to a file.
+        /// </summary>
+        private void convertButton_Click(object sender, EventArgs e)
+        {
+            // clear the output text box
+            outputTextBox.Text = string.Empty;
+
+            // holds the input text from the file
+            string inputText = String.Empty;
+            try
+            {
+                // open the input file and read in the contents to inputText
+                inputText = File.ReadAllText(inputFileTextBox.Text);
+            }
+            catch (IOException ex)
+            {
+                // output the error to the output text box
+                outputTextBox.Text += ex.ToString() + "\n";
+            }
+
+            // if there was no text read in, just return
+            if (String.IsNullOrEmpty(inputText))
+            {
+                // display to the user that there was not text to convert
+                outputTextBox.Text += "There was nothing to convert from the input file." + "\n";
+                return;
+            }
+  
+            // split out the text for each new line
+            string[] keyPaths = inputText.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+            List<string> outputText = new List<string>();
+
+            // loop through the lines in the input file
+            foreach (string keyPath in keyPaths)
+            {
+                // if there is anything to convert
+                if (keyPath.Length > 0)
+                {
+                    // add the converted text to outputText
+                    outputText.Add(ConvertPath(keyPath));
+                }
+            }
+
+            // if there is anything to output
+            if (outputText.Count > 0)
+            {
+                try
+                {
+                    // open the output file and write out the contents.  This will erase the contents of the file
+                    File.WriteAllLines(outputFileTextBox.Text, outputText.ToArray());
+                }
+                catch (IOException ex)
+                {
+                    // output the error to the output text box
+                    outputTextBox.Text += ex.ToString() + "\n"; 
+                }
+
+                // display the converted string
+                outputTextBox.Text += string.Join("\r\n", outputText);
+            }
+        }
+        /// <summary>
+        /// Converts the given keyPath characters to text based on the contents of the remoteKeys array
+        /// </summary>
+        /// <param name="keyPath">the keyPath to convert</param>
+        /// <returns>string representing the keyPath</returns>
+        private string ConvertPath(string keyPath)
+        {
+            // reset the current row and columns
+            currentRow = currentCol = 0;
+
+            // if there is nothing to convert, return an empty string
+            if (String.IsNullOrEmpty(keyPath))
+                return String.Empty;
+
+            StringBuilder convertedPath = new StringBuilder();
+
+            // loop through each character in the keyPath
+            for (int i = 0; i < keyPath.Length; i++)
+            {
+                // determine the character
+                switch (keyPath[i])
+                {
+                    // UP
+                    case 'U':
+                        // make sure we are not going out of row bounds
+                        if (currentRow == 0)
+                            currentRow = 5;
+                        else
+                            currentRow -= 1;
+                        break;
+                    // DOWN
+                    case 'D':
+                        // make sure we are not going out of row bounds
+                        if (currentRow == 5)
+                            currentRow = 0;
+                        else
+                            currentRow += 1;
+                        break;
+                    // LEFT
+                    case 'L':
+                        // make sure we are not going out of column bounds
+                        if (currentCol == 0)
+                            currentCol = 5;
+                        else
+                            currentCol -= 1;
+                        break;
+                    // RIGHT
+                    case 'R':
+                        // make sure we are not going out of column bounds
+                        if (currentCol == 5)
+                            currentCol = 0;
+                        else
+                            currentCol += 1;
+                        break;
+                    // SPACE
+                    case 'S':
+                        // add a space character
+                        convertedPath.Append(" ");
+                        break;
+                    // ENTER
+                    case '*':
+                        // enter the selected character at the current position
+                        convertedPath.Append(remoteKeys[currentRow, currentCol]);
+                        break;
+                    // ignore all other characters
+                    default:
+                        break;
+                }
+            }
+
+            // return the converted string
+            return convertedPath.ToString();
+        }
+    }
+}
